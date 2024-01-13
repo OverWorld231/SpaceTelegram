b0bb8845255f4562c0c1783a80e26bc32ce372b1
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2024-01-13 19:53:37 +0300

    Оформил код

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,9 +1,18 @@
 import requests
 from download import download_image
-def fetch_spacex_last_launch():
-    response = requests.get("https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a")
 
+
+def fetch_spacex_last_launch():
+    response = requests.get(
+        "https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a")
     spacex_urls = response.json()["links"]["flickr"]["original"]
     for number, spacex_url in enumerate(spacex_urls):
-        download_image(spacex_url,f"images/spacex{number}.jpg")
-fetch_spacex_last_launch()
+        download_image(spacex_url, f"images/spacex{number}.jpg")
+
+
+def main():
+    fetch_spacex_last_launch()
+
+
+if __name__ == "__main__":
+    main()
