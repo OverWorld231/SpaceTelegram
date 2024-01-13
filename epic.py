8cfcc43e8b15abd0be6557844ecaa46fb61e7e2a
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2024-01-13 19:51:48 +0300

    Оформил код

--- epic.py
+++ epic.py
@@ -1,18 +1,30 @@
 import requests
 import datetime
 import os
-from download import download_image 
-def earth_images():
-  epic_link = "https://api.nasa.gov/EPIC/api/natural"
-  api_key = os.environ['NASA_API_KEY']
-  params = {"api_key":api_key,"count": 5}
-  response = requests.get(epic_link,params=params)
-  earth_url = response.json()
-  for epic_url in earth_url:
-    epic_date = epic_url["date"]
-    epic_date = datetime.datetime.fromisoformat(epic_date).strftime("%Y/%m/%d")
-    epic_name = epic_url["image"]
-    epic_link = f"https://api.nasa.gov/EPIC/archive/natural/{epic_date}/png/{epic_name}.png"
-    path = os.path.join("images",f"{epic_name}.png")
-    download_image(epic_link,path,params)
-earth_images()
+from download import download_image
+
+
+def earth_images(api_key):
+    epic_link = "https://api.nasa.gov/EPIC/api/natural"
+    count = 5
+    params = {"api_key": api_key, "count": count}
+    response = requests.get(epic_link, params=params)
+    response.raise_for_status()
+    earth_url = response.json()
+    for epic_url in earth_url:
+        epic_date = epic_url["date"]
+        epic_date = datetime.datetime.fromisoformat(epic_date).strftime(
+            "%Y/%m/%d")
+        epic_name = epic_url["image"]
+        epic_link = f"https://api.nasa.gov/EPIC/archive/natural/{epic_date}/png/{epic_name}.png"
+        path = os.path.join("images", f"{epic_name}.png")
+        download_image(epic_link, path, params)
+
+
+def main():
+    api_key = os.environ['NASA_API_KEY']
+    earth_images(api_key)
+
+
+if __name__ == "__main__":
+    main()
