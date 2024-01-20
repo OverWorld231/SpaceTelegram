ad83cc1b9e64fc6f50b6d78cee13538f34662ab7
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2024-01-20 18:18:37 +0300

    Проверил статус ответа

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,6 +5,7 @@ from download import download_image
 def fetch_spacex_last_launch():
     response = requests.get(
         "https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a")
+    response.raise_for_status()
     spacex_urls = response.json()["links"]["flickr"]["original"]
     for number, spacex_url in enumerate(spacex_urls):
         download_image(spacex_url, f"images/spacex{number}.jpg")
