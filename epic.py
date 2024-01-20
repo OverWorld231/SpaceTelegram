980548fa357d22a4528daabf8641d04ce6cb28ba
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2024-01-20 18:53:02 +0300

    Изменил название переменной

--- epic.py
+++ epic.py
@@ -10,12 +10,12 @@ def load_earth_images(api_key):
     params = {"api_key": api_key, "count": count}
     response = requests.get(epic_link, params=params)
     response.raise_for_status()
-    earth_url = response.json()
-    for epic_url in earth_url:
-        epic_date = epic_url["date"]
+    earth_img = response.json()
+    for epic_img in earth_img:
+        epic_date = epic_img["date"]
         epic_date = datetime.datetime.fromisoformat(epic_date).strftime(
             "%Y/%m/%d")
-        epic_name = epic_url["image"]
+        epic_name = epic_img["image"]
         epic_link = f"https://api.nasa.gov/EPIC/archive/natural/{epic_date}/png/{epic_name}.png"
         path = os.path.join("images", f"{epic_name}.png")
         download_image(epic_link, path, params)
