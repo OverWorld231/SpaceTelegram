f1388f781b966950ef16ec77d3a92f1668fa5102
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2024-01-20 19:03:32 +0300

    Добавил запросы по id

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -2,9 +2,9 @@ import requests
 from download import download_image
 
 
-def fetch_spacex_last_launch():
+def fetch_spacex_last_launch(launch_id):
     response = requests.get(
-        "https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a")
+        f"https://api.spacexdata.com/v5/launches/{launch_id}")
     response.raise_for_status()
     spacex_urls = response.json()["links"]["flickr"]["original"]
     for number, spacex_url in enumerate(spacex_urls):
@@ -12,7 +12,17 @@ def fetch_spacex_last_launch():
 
 
 def main():
-    fetch_spacex_last_launch()
+    parser = argparse.ArgumentParser(
+        description='Эта программа позволит вам загрузить фотографии с запуска SpaceX.'
+    )
+    parser.add_argument(
+        '--id',
+        dest='launch_id',
+        default="5eb87d47ffd86e000604b38a",
+        help='Укажите ID запуска SpaceX, с которого можно загрузить фотографии.'
+    )
+    args = parser.parse_args()
+    fetch_spacex_last_launch(args.launch_id)
 
 
 if __name__ == "__main__":
